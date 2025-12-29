104f393b62614cbd2a9b3c52cbd6f4d1cb520de0
10qDanya <lumusyt@gmail.com>
2025-12-29 19:52:58 +0300

    updated urls.py added html urls

--- color_app/urls.py
+++ color_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/color/', views.ColorChangeView.as_view(), name='color_change'),
     path('api/user/', views.UserInfoView.as_view(), name='user_info'),
+
+    #HTML страницы
+    path('', views.index, name='index'),
+    path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register_page'),
 ]
